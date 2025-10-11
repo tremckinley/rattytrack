e29de25fd5e6b2c00a408a1d4c3f1d393375c212
tremckinley <tremckinley901@gmail.com>
2025-10-11 00:06:33 -0500

    First Commit with navbar

---
+++ app/latinas/page.tsx
@@ -0,0 +1,3 @@
+export default function Latinas() {
+    return <div>Hola, seniorita</div>
+}
---
+++ components/NavBar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+import Image from "next/image"
+
+export default function NavBar() {
+    return (
+        <nav className="flex items-center shadow-sm py-4 mb-2 bg-red-100">
+        <Image
+            className="ml-4"
+            src='/burgundy_logo.png'
+            alt='capytrack logo'
+            width={100}
+            height={50}
+        />
+        <span className="font-extrabold">CapyTrackAI</span>
+            <div className="px-2">
+            <Link className="mx-1 rounded-sm p-1 hover:bg-red-600" href="/">Home</Link>
+            <Link className="mx-1 rounded-sm p-1 hover:bg-red-600" href="/latinas">Latinas</Link>
+            </div>
+        </nav>
+    )
+}
