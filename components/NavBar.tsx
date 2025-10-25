170df70e0672f7632ad26924c66c2f90bf72e4d2
Tremaine McKinley <49395726+tremckinley@users.noreply.github.com>
2025-10-25 03:28:21 +0000

    Navbar edits to finish

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -41,21 +41,18 @@ export default function NavBar() {
           {navLinks.map((link) => {
             const isActive = pathname.split("/")[1] === link.href.split("/")[1];
             return (
-              <li
-                className={
-                  isActive
-                    ? "hover:bg-gray-200 self-end pt-4 px-4 pb-1 active-nav-link"
-                    : "hover:bg-gray-200 self-end pt-4 px-4 pb-1"
-                }
-                key={link.name}
-              >
-                <Link
-                  href={link.href}
-                  
+              <Link href={link.href}>
+                <li
+                  className={
+                    isActive
+                      ? "hover:bg-gray-200 self-end pt-4 px-4 pb-1 active-nav-link"
+                      : "hover:bg-gray-200 self-end pt-4 px-4 pb-1"
+                  }
+                  key={link.name}
                 >
                   {link.name}
-                </Link>
-              </li>
+                </li>
+              </Link>
             );
           })}
         </ul>
